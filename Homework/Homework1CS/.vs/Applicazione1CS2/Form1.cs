36f2e183d2ff44dc8425dd642a4432aac600d96a
R4Lpf <a.r.almoneda@gmail.com>
2022-10-04 23:20:45 +0200

    final update

---
+++ Homework/Homework1CS/.vs/Applicazione1CS2/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using static System.Security.Cryptography.RandomNumberGenerator;
+
+namespace Applicazione1CS2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random r = new Random();
+        public int Gen(){
+            return r.Next(1,7);
+        }
+
+        int[] count = { 0, 0, 0, 0, 0, 0 };
+
+        private void buttonGenerate_Click(object sender, EventArgs e)
+        {
+            int a = Gen();
+            int s = Gen();
+            int d = Gen();
+            int f = Gen();
+            int g = Gen();
+            int h = Gen();
+            label1.Text = a.ToString();
+            label2.Text = s.ToString();
+            label3.Text = d.ToString();
+            label4.Text = f.ToString();
+            label5.Text = g.ToString();
+            label6.Text = h.ToString();
+
+            string caratteri = a.ToString() + s.ToString() + d.ToString() + f.ToString() + g.ToString() + h.ToString();
+
+            
+
+            for (int i = 0; i < caratteri.Length; i++) {
+                if (caratteri[i] == '1') {
+                    count[0] += 1;
+                }
+                else if (caratteri[i] == '2')
+                {
+                    count[1] += 1;
+                }
+                else if (caratteri[i] == '3')
+                {
+                    count[2] += 1;
+                }
+                else if (caratteri[i] == '4')
+                {
+                    count[3] += 1;
+                }
+                else if (caratteri[i] == '5')
+                {
+                    count[4] += 1;
+                }
+                else if (caratteri[i] == '6')
+                {
+                    count[5] += 1;
+                }
+
+            }
+
+            foreach (var series in chart1.Series)
+            {
+                series.Points.Clear();
+            }
+
+            this.chart1.Series["Count"].Points.AddXY("1", count[0]);
+            this.chart1.Series["Count"].Points.AddXY("2", count[1]);
+            this.chart1.Series["Count"].Points.AddXY("3", count[2]);
+            this.chart1.Series["Count"].Points.AddXY("4", count[3]);
+            this.chart1.Series["Count"].Points.AddXY("5", count[4]);
+            this.chart1.Series["Count"].Points.AddXY("6", count[5]);
+
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
--- Homework/Homework1VB/Homework1VB/Form1.Designer.vb
+++ Homework/Homework1VB/Homework1VB/Form1.Designer.vb
@@ -22,9 +22,9 @@ Partial Class Form1
     'Non modificarla mediante l'editor del codice.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Dim ChartArea2 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
-        Dim Legend2 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend()
-        Dim Series2 As System.Windows.Forms.DataVisualization.Charting.Series = New System.Windows.Forms.DataVisualization.Charting.Series()
+        Dim ChartArea1 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
+        Dim Legend1 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend()
+        Dim Series1 As System.Windows.Forms.DataVisualization.Charting.Series = New System.Windows.Forms.DataVisualization.Charting.Series()
         Me.n1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Button1 = New System.Windows.Forms.Button()
@@ -111,18 +111,18 @@ Partial Class Form1
         '
         'Chart1
         '
-        ChartArea2.Name = "ChartArea1"
-        Me.Chart1.ChartAreas.Add(ChartArea2)
-        Legend2.Name = "Legend1"
-        Me.Chart1.Legends.Add(Legend2)
+        ChartArea1.Name = "ChartArea1"
+        Me.Chart1.ChartAreas.Add(ChartArea1)
+        Legend1.Name = "Legend1"
+        Me.Chart1.Legends.Add(Legend1)
         Me.Chart1.Location = New System.Drawing.Point(294, 60)
         Me.Chart1.Name = "Chart1"
-        Series2.ChartArea = "ChartArea1"
-        Series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Radar
-        Series2.Legend = "Legend1"
-        Series2.Name = "Count"
-        Series2.YValuesPerPoint = 4
-        Me.Chart1.Series.Add(Series2)
+        Series1.ChartArea = "ChartArea1"
+        Series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Radar
+        Series1.Legend = "Legend1"
+        Series1.Name = "Count"
+        Series1.YValuesPerPoint = 4
+        Me.Chart1.Series.Add(Series1)
         Me.Chart1.Size = New System.Drawing.Size(427, 324)
         Me.Chart1.TabIndex = 8
         Me.Chart1.Text = "Chart1"
@@ -140,6 +140,7 @@ Partial Class Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
         Me.ClientSize = New System.Drawing.Size(800, 450)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Chart1)
