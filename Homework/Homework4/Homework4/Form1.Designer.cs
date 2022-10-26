6040b96026e91a9d2db281e774bf0477a9e98fdd
R4Lpf <a.r.almoneda@gmail.com>
2022-10-26 13:00:45 +0200

    homework 4
    asdf

---
+++ Homework/Homework4/Homework4/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace Homework4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.R = new System.Windows.Forms.Button();
+            this.A = new System.Windows.Forms.Button();
+            this.N = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart3 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(15, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(557, 539);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // R
+            // 
+            this.R.Location = new System.Drawing.Point(311, 557);
+            this.R.Name = "R";
+            this.R.Size = new System.Drawing.Size(263, 48);
+            this.R.TabIndex = 2;
+            this.R.Text = "Relative Frequency";
+            this.R.UseVisualStyleBackColor = true;
+            this.R.Click += new System.EventHandler(this.R_Click);
+            // 
+            // A
+            // 
+            this.A.Location = new System.Drawing.Point(12, 557);
+            this.A.Name = "A";
+            this.A.Size = new System.Drawing.Size(293, 48);
+            this.A.TabIndex = 3;
+            this.A.Text = "Absolute Frequency";
+            this.A.UseVisualStyleBackColor = true;
+            this.A.Click += new System.EventHandler(this.A_Click);
+            // 
+            // N
+            // 
+            this.N.Location = new System.Drawing.Point(580, 557);
+            this.N.Name = "N";
+            this.N.Size = new System.Drawing.Size(282, 48);
+            this.N.TabIndex = 4;
+            this.N.Text = "Normalized Frequency";
+            this.N.UseVisualStyleBackColor = true;
+            this.N.Click += new System.EventHandler(this.N_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(868, 557);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(276, 48);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Generate";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(562, 545);
+            this.label1.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(559, 538);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(547, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(25, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "100";
+            // 
+            // chart1
+            // 
+            chartArea1.AxisY.Maximum = 100D;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(595, 9);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Excel;
+            series1.ChartArea = "ChartArea1";
+            series1.Color = System.Drawing.Color.White;
+            series1.Legend = "Legend1";
+            series1.Name = "Absolute Frequency";
+            series1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Pastel;
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(549, 176);
+            this.chart1.TabIndex = 9;
+            this.chart1.Text = "chart1";
+            // 
+            // chart2
+            // 
+            chartArea2.AxisY.Maximum = 100D;
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart2.Legends.Add(legend2);
+            this.chart2.Location = new System.Drawing.Point(595, 191);
+            this.chart2.Name = "chart2";
+            series2.ChartArea = "ChartArea1";
+            series2.Color = System.Drawing.Color.White;
+            series2.Legend = "Legend1";
+            series2.Name = "Relative Frequency";
+            series2.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.SemiTransparent;
+            this.chart2.Series.Add(series2);
+            this.chart2.Size = new System.Drawing.Size(549, 172);
+            this.chart2.TabIndex = 10;
+            this.chart2.Text = "chart2";
+            // 
+            // chart3
+            // 
+            chartArea3.AxisY.Maximum = 100D;
+            chartArea3.Name = "ChartArea1";
+            this.chart3.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart3.Legends.Add(legend3);
+            this.chart3.Location = new System.Drawing.Point(595, 369);
+            this.chart3.Name = "chart3";
+            series3.ChartArea = "ChartArea1";
+            series3.Color = System.Drawing.Color.Moccasin;
+            series3.Legend = "Legend1";
+            series3.Name = "Normalized Frequency";
+            this.chart3.Series.Add(series3);
+            this.chart3.Size = new System.Drawing.Size(549, 182);
+            this.chart3.TabIndex = 11;
+            this.chart3.Text = "chart3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1156, 617);
+            this.Controls.Add(this.chart3);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.N);
+            this.Controls.Add(this.A);
+            this.Controls.Add(this.R);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button R;
+        private System.Windows.Forms.Button A;
+        private System.Windows.Forms.Button N;
+        private System.Windows.Forms.Button button4;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart3;
+    }
+}
+
---
+++ Homework/Homework4/Homework4/Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using static System.Windows.Forms.AxHost;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
+
+namespace Homework4
+{
+    
+    public partial class Form1 : Form
+    {
+        public Chart chart;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void A_Click(object sender, EventArgs e)
+        {
+            Global.IsRelative = false;
+            Global.IsAbsolute = true;
+            Global.IsNormalized = false;
+
+            chart = new Chart(pictureBox1.Width, pictureBox1.Height, Global.TRIAL_NUMBER, Global.TRIAL_NUMBER);
+            pictureBox1.Image = chart._Bitmap;
+            foreach (DiceDistribution dice in Global.DiceCollection)
+            {
+                chart.InsertCollection(dice.AbsoluteFrequency, dice._Color);
+            }
+
+        }
+        private void R_Click(object sender, EventArgs e)
+        {
+            Global.IsRelative = true;
+            Global.IsAbsolute = false;
+            Global.IsNormalized = false;
+
+            chart = new Chart(pictureBox1.Width, pictureBox1.Height, Global.TRIAL_NUMBER, Global.PERCENTAGE_ACCURACY);
+            pictureBox1.Image = chart._Bitmap;
+            foreach (DiceDistribution dice in Global.DiceCollection)
+            {
+                chart.InsertCollection(dice.RelativeFrequency, dice._Color);
+            }
+        }
+
+        private void N_Click(object sender, EventArgs e)
+        {
+            Global.IsRelative = false;
+            Global.IsAbsolute = false;
+            Global.IsNormalized = true;
+
+            chart = new Chart(pictureBox1.Width, pictureBox1.Height, Global.TRIAL_NUMBER, Global.PERCENTAGE_ACCURACY);
+            pictureBox1.Image = chart._Bitmap;
+            foreach (DiceDistribution dice in Global.DiceCollection)
+            {
+                chart.InsertCollection(dice.NormalizedFrequency, dice._Color);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Global.DiceCollection = new List<DiceDistribution>();
+
+            for (int i = 0; i < Global.COLLECTION_NUMBER; i++)
+            {
+                DiceDistribution dice = new DiceDistribution(Global.TRIAL_NUMBER, Global.PERCENTAGE_ACCURACY);
+                Global.DiceCollection.Add(dice);
+                System.Threading.Thread.Sleep(10);
+                this.chart1.Series["Absolute Frequency"].Points.AddXY(i+1, (int) dice.AbsoluteFrequency[9999].Item2/100);
+                this.chart2.Series["Relative Frequency"].Points.AddXY(i+1, (int) dice.RelativeFrequency[9999].Item2 / 100);
+                this.chart3.Series["Normalized Frequency"].Points.AddXY(i+1, (int) dice.NormalizedFrequency[9999].Item2 / 100);
+            }
+
+            if (Global.IsAbsolute) A_Click(sender, e);
+            else if (Global.IsRelative) R_Click(sender, e);
+            else N_Click(sender, e);
+        }
+
+       
+    }
+
+    public class Global
+    {
+        //public static int PERCENTAGE_ACCURACY = 1000000;
+        //public static int TRIAL_NUMBER = 10000;
+        //public static int COLLECTION_NUMBER = 4;
+
+        public static int PERCENTAGE_ACCURACY = 10000;
+        public static int TRIAL_NUMBER = 10000;
+        public static int COLLECTION_NUMBER = 10;
+        public static List<DiceDistribution> DiceCollection = new List<DiceDistribution>();
+
+        public static bool IsAbsolute = true;
+        public static bool IsRelative = false;
+        public static bool IsNormalized = false;
+    }
+
+    public class DiceDistribution
+    {
+        public Color _Color { get; set; }
+        public int TrialNumber { get; set; }
+        public int PercentageAccuracy { get; set; }
+        public List<(int, int)> AbsoluteFrequency { get; set; }
+        public List<(int, int)> RelativeFrequency { get; set; }
+        public List<(int, int)> NormalizedFrequency { get; set; }
+        public DiceDistribution(int trialNumber, int percentageAccuracy)
+        {
+            Random rand = new Random();
+
+            _Color = Color.FromArgb(rand.Next(0, 256), rand.Next(0, 256), rand.Next(0, 256));
+            TrialNumber = trialNumber;
+            PercentageAccuracy = percentageAccuracy;
+            AbsoluteFrequency = new List<(int, int)>();
+            RelativeFrequency = new List<(int, int)>();
+            NormalizedFrequency = new List<(int, int)>();
+
+            int head = 0;
+            Debug.Write($"\n");
+            for (int i = 0; i < TrialNumber; i++)
+            {
+                if (rand.Next(0, 2) == 1)
+                    head += 1;
+                int percent = (int)((float)head / ((float)i + 1) * PercentageAccuracy);
+                int normalized = (int)((float)head / ((float)Math.Sqrt(i + 1)) * (PercentageAccuracy / 100));
+
+
+
+                
+                AbsoluteFrequency.Add((i + 1, head));
+                RelativeFrequency.Add((i + 1, percent));
+                NormalizedFrequency.Add((i + 1, normalized));
+            }
+        }
+    }
+
+    public class Chart
+    {
+        public Bitmap _Bitmap { get; private set; }
+        public Graphics _Graphics { get; private set; }
+        private List<List<(int, int)>> Series { get; set; }
+        public int MaxX { get; set; }
+        public int MaxY { get; set; }
+        public int MinX { get; set; }
+        public int MinY { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public Chart(int width, int height, int x, int y)
+        {
+            _Bitmap = new Bitmap(width, height);
+            _Graphics = Graphics.FromImage(_Bitmap);
+            Series = new List<List<(int, int)>>();
+
+            Width = width;
+            Height = height;
+
+            MaxX = x;
+            MaxY = y;
+            MinX = 0;
+            MinY = 0;
+        }
+
+        public void DrawLine(int x1, int y1, int x2, int y2, Pen pen)
+        {
+            _Graphics.DrawLine(pen, x1, y1, x2, y2);
+        }
+        public void DrawLine((int, int) p1, (int, int) p2, Pen pen)
+        {
+            _Graphics.DrawLine(pen, p1.Item1, p1.Item2, p2.Item1, p2.Item2);
+        }
+
+        public (int, int) ConvertCoordinates((int, int) point)
+        {
+            float x = ((float)(point.Item1 - MinX) / (float)(MaxX - MinX)) * Width;
+            float y = Height - (((float)(point.Item2 - MinY) / (float)(MaxY - MinY)) * Height);
+
+            return ((int)x, (int)y);
+        }
+
+        public void InsertCollection(List<(int, int)> collection, Color color)
+        {
+            Series.Add(collection);
+            (int, int) buffer = collection[0];
+            Pen pen = new Pen(color, 1);
+
+            foreach ((int, int) item in collection.Skip(1))
+            {
+                DrawLine(ConvertCoordinates(buffer), ConvertCoordinates(item), pen);
+                buffer = item;
+            }
+        }
+
+        public void ComputeCandles(int x, int thickness, int maxLength, Color color)
+        {
+            Pen pen = new Pen(color, thickness);
+            int startY = MaxY / 2;
+            //if (Series.Count % 2 == 0) { startY += thickness + 3 + (((Series.Count / 2) - 1) * ((thickness * 2) + 3)); }
+            //else { startY += (Series.Count / 2)  * ((Series.Count - 1) * ((thickness * 2) + 3)); }
+
+            foreach (List<(int, int)> value in Series)
+            {
+                int c1 = value[x].Item1;
+                int c2 = value[x].Item2;
+
+                //Debug.WriteLine($"C1: {x} - C2: {c2}");
+                //Debug.WriteLine($"C1: {c1} - C2: {c2}");
+                //Debug.WriteLine($"-----------------");
+
+                Debug.WriteLine($"X: {x} - X: {startY}");
+                Debug.WriteLine($"X: {maxLength + x} - X: {startY}");
+
+                DrawLine(ConvertCoordinates((x, startY)), ConvertCoordinates((maxLength + x, startY)), new Pen(Color.Red, thickness));
+                startY -= ((thickness * 2) + 3);
+            }
+        }
+    }
+}
